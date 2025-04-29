71d91a90f17d291d7d04a124f0a99d072eb43809
dong1270 <kang1270ds@gmail.com>
2025-04-30 00:13:14 +0900

    test: led test

---
+++ test_led.py
@@ -0,0 +1,46 @@
+import os
+
+def run() :
+    allow_try = 0
+    cmd = ""
+    ledChk = 0
+    while True:
+        if ledChk == 0:
+            os.system("./led_src/y_led_on.run")
+            ledChk = 1
+        else :
+            ledChk = 0
+            os.system("./led_src/y_led_off.run")
+
+        if allow_try == 50:
+            cmd = "none"
+            print('다름')
+            break
+        confidence = int(input("test: "))
+        if confidence > 76:
+            print("unlock")
+            cmd = "pass"
+            break
+        else:
+         #75 이하면 타인.. Locked!!!
+            print("lock")
+            allow_try += 1
+
+        allow_try += 1
+        print(allow_try)
+
+    os.system("./led_src/fron_door.run " + cmd)
+
+    if ledChk == 1:
+        os.system("./led_src/y_led_off.run")
+        
+if __name__ == "__main__":
+    #if isNull() == 0 :
+        # 학습 시작
+    #    models = trains()
+    #else :
+        # 학습 모델 가져오기
+     #   models = importModel()
+
+    # 얼굴인식
+    run()
